afadf5075212531388d26b4f829399a62032a2e0
Douglas Oliveira <douglaspo_97@outlook.com>
2022-06-25 23:10:50 -0300

    feat: creating ui components

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'suaapikey',
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4onyax50hic01xx4eyg4jjg/master',
   cache: new InMemoryCache()
 })
