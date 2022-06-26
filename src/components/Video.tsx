fcb76ba0f0983d921107629a4646170eaa103942
Douglas Oliveira <douglaspo_97@outlook.com>
2022-06-26 20:25:44 -0300

    feat: subscriber mutation

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -8,7 +8,7 @@ const GET_LESSON_BY_SLUG_QUERY = gql`
   query GetLessonBySlug ($slug: String) {
     lesson(where: {slug: $slug}) {
       title
-      id
+      videoId
       description
       teacher {
         bio
